0f84b8e4621efcad96bfb0551bd61230b78d2bd3
Unknown <real83@hotmail.fr>
2018-05-11 16:35:52 +0200

    Map
    Not working

---
+++ Projet ASPnet/Isen.DotNet.Web/Controllers/MapController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Isen.DotNet.Web.Models;
+
+namespace Isen.DotNet.Web.Controllers
+{
+    public class MapController : Controller
+    {
+        public IActionResult Map()
+        {
+
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- Projet ASPnet/Isen.DotNet.Web/wwwroot/js/script.js
+++ Projet ASPnet/Isen.DotNet.Web/wwwroot/js/script.js
@@ -1,4 +1,4 @@
-﻿var map;
+var map;
 
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
